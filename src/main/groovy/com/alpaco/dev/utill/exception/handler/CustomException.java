53c83ecd9b2dfc236046be8d5f3e6cfaf90ec86e
naaaaeun <naeunlee1223@gmail.com>
2023-05-30 19:02:02 +0900

    init

---
+++ src/main/groovy/com/alpaco/dev/utill/exception/handler/CustomException.java
@@ -0,0 +1,39 @@
+package com.alpaco.dev.utill.exception.handler;
+
+import com.alpaco.dev.utill.BaseResponse;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.alpaco.dev.utill.BaseResponseStatus.EMPTY_REQUEST_PARAMETER;
+import static com.alpaco.dev.utill.BaseResponseStatus.INVALID_REQUEST;
+
+@Slf4j
+@RestControllerAdvice
+class CustomExceptionHandler {
+
+    // 파라미터가 없는 경우
+    @ExceptionHandler({MissingServletRequestParameterException.class})
+    public BaseResponse<Object> handleRequestParameter() {
+        return new BaseResponse<>(EMPTY_REQUEST_PARAMETER);
+    }
+
+    // Not Valid한 경우
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public BaseResponse<Object> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        log.info("errors = {}", e.getBindingResult().getFieldError().getDefaultMessage());
+
+        // forEach로 모든 에러 메세지를 던집니다.
+        e.getBindingResult().getAllErrors()
+                .forEach(c -> errors.put(((FieldError) c).getField(), c.getDefaultMessage()));
+        return new BaseResponse<>(INVALID_REQUEST, errors);
+    }
+
+}
