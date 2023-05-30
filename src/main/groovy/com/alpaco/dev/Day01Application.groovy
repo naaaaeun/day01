a0e43260b73a11e3a99e618a1f501e3b79139f9e
naaaaeun <naeunlee1223@gmail.com>
2023-05-30 18:49:37 +0900

    init

---
+++ src/main/groovy/com/alpaco/dev/Day01Application.groovy
@@ -0,0 +1,13 @@
+package com.alpaco.dev
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Day01Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(Day01Application, args)
+	}
+
+}
